7c77a11a1784c9fbc968eaf0a63a37ec3059c423
Rishi Pradhan <rishipradhan.dev@gmail.com>
2018-07-31 18:54:53 +0530

    FT now checks for more than one list item

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
