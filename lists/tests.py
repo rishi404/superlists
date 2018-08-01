afdbad97714fc5fb53bc6f37760a5cd726724dae
Rishi Pradhan <rishipradhan.dev@gmail.com>
2018-08-01 16:11:10 +0530

    Redirect after POST, and show all list items in template

--- lists/tests.py
+++ lists/tests.py
@@ -10,8 +10,28 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().text, 'A new list item')
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_item(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('Item 1', response.content.decode())
+        self.assertIn('Item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
-def home_page(request):
+def home_page(request: HttpRequest):
+    if request.method == 'POST':
+        item: Item = Item()
+        item.text = request.POST['item_text']
+        item.save()
+        return redirect('/')
+
+    items = Item.objects.all()
     return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'items': items,
     })
