1c75f41e20580c803b94c82137107d5937467be8
Rishi Pradhan <rishipradhan.dev@gmail.com>
2018-08-01 17:43:51 +0530

    New URL, view and template to display list

--- lists/views.py
+++ lists/views.py
@@ -9,9 +9,13 @@ def home_page(request: HttpRequest):
         item: Item = Item()
         item.text = request.POST['item_text']
         item.save()
-        return redirect('/')
+        return redirect('/lists/the-only-list/')
 
+    return render(request, 'lists/home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/home.html', {
+    return render(request, 'lists/list.html', {
         'items': items,
     })
