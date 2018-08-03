0a5170eb0db9f7c7c509f582e1e1662954423f15
Rishi Pradhan <rishipradhan.dev@gmail.com>
2018-08-03 19:55:33 +0530

    Moved FTs into their own individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,17 @@
+from .base import FunctionalTest
+
+
+class LayoutAndStyling(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the inputbox is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
